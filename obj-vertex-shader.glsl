65fa3fb242108086da510c414a93205ff9640c4e
Mostafa Razavi <mostafa@sepent.com>
2020-06-25 23:09:51 +0200

    Fix artefacts around objects.
    This is the same issue as the "background bleeding" problem a few
    commits back. Fixed with the same solution by offsetting the texture
    coordinates for a bit towards the inside of the texture slice.

--- obj-vertex-shader.glsl
+++ obj-vertex-shader.glsl
@@ -23,19 +23,19 @@ void main()
         switch (index) {
         case 0: // bottom-left
                 pos = obj_position;
-                texture_coords = obj_texture_coords.xy;
+                texture_coords = obj_texture_coords.xy + vec2(0.001, 0.001);
                 break;
         case 1: // top-left
                 pos = obj_position + vec2(0, obj_size.y);
-                texture_coords = obj_texture_coords.xw;
+                texture_coords = obj_texture_coords.xw + vec2(0.001, -0.001);
                 break;
         case 2: // top-right
                 pos = obj_position + obj_size;
-                texture_coords = obj_texture_coords.zw;
+                texture_coords = obj_texture_coords.zw + vec2(-0.001, -0.001);
                 break;
         case 3: // bottom-right
                 pos = obj_position + vec2(obj_size.x, 0);
-                texture_coords = obj_texture_coords.zy;
+                texture_coords = obj_texture_coords.zy + vec2(-0.001, 0.001);
                 break;
         }
 
