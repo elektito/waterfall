45f7231a2d262913fa709b179b83f7fb5af779d2
Mostafa Razavi <mostafa@sepent.com>
2020-06-25 17:59:22 +0200

    Fix background bleeding issue.
    This is a fix for an issue that sometimes happened where background
    color seemed to bleed through between map tiles. The reason this
    happens seemed to be that the texture is sampled a bit outside the
    tile area specified, sampling transparent values.
    
    The fix offsets texture coordinates by a tiny bit (0.001 at the
    moment) towards the inside of the tile.

--- map-vertex-shader.glsl
+++ map-vertex-shader.glsl
@@ -26,22 +26,28 @@ void main()
                     gl_InstanceID / map_width);
 
         // "index" determines which tile vertex we have.
+        //
+        // Note about texture coordinates: texture coordinates are
+        // offset by a small amount towards the inside of the tile,
+        // this is to get rid of a situation where the tiles do not
+        // completely mesh with each other, causing one pixel of
+        // background to occasionally bleed through.
         switch (index) {
         case 0: // bottom-left
                 pos = tile;
-                texture_coords = tile_texture_coords.xy;
+                texture_coords = tile_texture_coords.xy + vec2(0.001, 0.001);
                 break;
         case 1: // top-left
                 pos = tile + vec2(0, 1);
-                texture_coords = tile_texture_coords.xw;
+                texture_coords = tile_texture_coords.xw + vec2(0.001, -0.001);
                 break;
         case 2: // top-right
                 pos = tile + vec2(1, 1);
-                texture_coords = tile_texture_coords.zw;
+                texture_coords = tile_texture_coords.zw + vec2(-0.001, -0.001);
                 break;
         case 3: // bottom-right
-                pos = tile + vec2(1, 0);
-                texture_coords = tile_texture_coords.zy;
+                pos = tile + vec2(1, -0);
+                texture_coords = tile_texture_coords.zy + vec2(-0.001, 0.001);
                 break;
         }
 
